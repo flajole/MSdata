5c9f33b4403e9c4790ec61bd8eeb81342f0b2003
flajole <dmizubkov@gmail.com>
2015-09-07 02:35:56 +0300

    Docmentation added. Name matching in biomass.table and data set is fixed

--- R/Norm.R
+++ R/Norm.R
@@ -1,27 +1,45 @@
 setGeneric("Norm", 
            function(msdata, ...) standardGeneric("Norm"))
-
+#' MSdata normalisation
+#'
+#' Sample-wise normalisations.
+#' @param msdata \code{\link{MSdata-class}} object
+#' @param method Method of normalisation. In each sample data are normalised by one of:\cr
+#' \code{"Sum"} -  sum concentration of all compounds\cr
+#' \code{"Median"} - median concentration of all compounds\cr
+#' \code{"RefCompound"} - concentration of reference compound\cr
+#' \code{"Biomass"} - sample biomass/volume/etc. \cr
+#' @param biomass.list For \code{"Biomass"} method, the link to the file containing the simple table with two columns: sampleID and biomasses/volumes/etc.
+#' @param ref.cmpd For \code{"RefCompound"} method, the name or number of reference compound.
+#' 
+#' @return \code{\link{MSdata-class}} object with normalised intensity matrix
+#' @export 		   	
 setMethod("Norm", "MSdata",
           function(msdata,
                    method,
                    biomass.list = NULL,
                    ref.cmpd = NULL) {
-              .intMatrix <- msdata@intMatrix
               match.arg(method, c("Biomass", "Sum", "Median", "RefCompound"))
+			  .intMatrix <- msdata@intMatrix
               
               if (method == "Biomass") {
                   if (is.null(biomass.list)) 
                       stop("Method is set to normalisation by biomass, 
                            but biomasses list is not selected!")
-                  biomass.table <- read.table(biomass.list, sep="\t", col.names = c("Sample", "Biomass"))
-                  if (all(standards.table[,"rt"] < 60)) standards.table[,"rt"] <- standards.table[,"rt"]*60
-                  
-                  masses <- matrix(biomass.table[["Biomass"]],
+                  biomass.table <- suppressWarnings(read.table(biomass.list, sep="\t", row.names = 1, col.names = c("Biomass")))
+				  
+				  miss.samples <- setdiff(colnames(.intMatrix), rownames(biomass.table))  
+				  if (length(miss.samples) > 0))
+					  stop("Not all sample names are found in biomass table!")
+					  
+				  biomass.table <- biomass.table[colnames(.intMatrix), ]  
+				  
+                  masses <- matrix(biomass.table,
                                    nrow=nrow(.intMatrix), 
                                    ncol=ncol(.intMatrix), byrow=TRUE)
-                  meanMass <- mean(biomass.table["Biomass"])
+                  meanMass <- mean(biomass.table)
                   .intMatrix <- .intMatrix * meanMass / masses 
-                  msg <- paste0("Data are normalised by biomass, file used as list: ", biomass.list)
+                  msg <- paste0("Data are normalised by biomass; the file used as list: ", biomass.list)
                   
               } else if (method == "Median") {
                   medians <- matrix(apply(.intMatrix, 2, median, na.rm = TRUE)
@@ -45,6 +63,8 @@ setMethod("Norm", "MSdata",
                   if (is.null(ref.cmpd)) 
                       stop("Method is set to normalisation by feature, 
                            but reference feature is not selected!")
+                  if (!(ref.cmpd %in% rownames(.intMatrix)) & !(ref.cmpd %in% 1:nrow(.intMatrix))) 
+                      stop("Reference feature is not found!")
                   refints <- matrix(.intMatrix[ref.cmpd, ],
                                     nrow=nrow(.intMatrix), 
                                     ncol=ncol(.intMatrix), 
