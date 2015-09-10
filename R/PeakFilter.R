833b1165acd765678529dced5f08cc7ccce400b9
flajole <dmizubkov@gmail.com>
2015-09-10 13:03:33 +0300

    Basic filtering from MetaboAnalyst is implemented

--- R/PeakFilter.R
+++ R/PeakFilter.R
@@ -35,46 +35,47 @@ setMethod("PeakFilter", "MSdata",
                    above.blank = 2,
                    min.int = 1000,
                    min.nonNAnum.repgroup = 3,
-				   min.nonNApercent = 0.4){
-			  fold.blank<-NULL
+                   min.nonNApercent = 0.4){
+              fold.blank<-NULL #temporary
+              msg <- c()
               .intMatrix <- intMatrix(msdata)
               se <- function(x) sqrt(var(x, na.rm = TRUE)/length(na.omit(x)))
               repapply <- function(foo) {
                   apply(.intMatrix, 1, function(peak) tapply(peak, reps, foo, peak))
               }
-			  
-#               rep.apply <- function(foo) {
-#                   sapply(levels(reps), function(repGroup)
-#                          apply(.intMatrix[ , reps == repGroup], 1, foo))
-#               }
-
-			  if (!is.null(blanks)) {
-					blank.intMatrix <- .intMatrix[ , blank]
-					.intMatrix <- .intMatrix[ , -blank]
-					reps  <- msdata@sampleData[-blanks, ]$ReplicationGroup
-			  } else {
-					reps  <- msdata@sampleData$ReplicationGroup
-			  }
-			  filt  <- array(dim = c(length(levels(reps)), nrow(.intMatrix), 0))
-			  
-
-			  
+              
+              #               rep.apply <- function(foo) {
+              #                   sapply(levels(reps), function(repGroup)
+              #                          apply(.intMatrix[ , reps == repGroup], 1, foo))
+              #               }
+              
+              if (!is.null(blanks)) {
+                  blank.intMatrix <- .intMatrix[ , blank]
+                  .intMatrix <- .intMatrix[ , -blank]
+                  reps  <- msdata@sampleData[-blanks, ]$ReplicationGroup
+              } else {
+                  reps  <- msdata@sampleData$ReplicationGroup
+              }
+              filt  <- array(dim = c(length(levels(reps)), nrow(.intMatrix), 0))
+              
+              
+              
               if (is.null(blanks) & (!is.null(fold.blank) | !is.null(above.blank))) {
                   warning("Blank samples are not selected, filtering by blanks has not been conducted")
-              
-			  } else if ((length(setdiff(blanks, colnames(.intMatrix))) != 0) &
-                         (length(setdiff(blanks, 1:ncol(.intMatrix)))   != 0)) {
+                  
+              } else if ((length(setdiff(blanks, colnames(.intMatrix))) != 0) &
+                             (length(setdiff(blanks, 1:ncol(.intMatrix)))   != 0)) {
                   warning("Blank samples set does not match sample names or numbers. Filtering by blanks will not be conducted")
-              
-			  } else {       
+                  
+              } else {       
                   if (!is.null(above.blank) & !is.null(blanks)) {
                       filt <- abind::abind(along = 3, filt, repapply(function(peakgr, peak) 
                           mean(peakgr, na.rm = TRUE) - above.blank * se(peakgr) >
-                          mean(blank.intMatrix, na.rm = TRUE) + above.blank * se(blank.intMatrix)))
+                              mean(blank.intMatrix, na.rm = TRUE) + above.blank * se(blank.intMatrix)))
                   }
                   # if (!is.null(fold.blank) & !is.null(blanks)) {
-                      # filt <- abind::abind(along = 3, filt, repapply(function(repGroup, peak)
-                          # TRUE))
+                  # filt <- abind::abind(along = 3, filt, repapply(function(repGroup, peak)
+                  # TRUE))
                   # }
               }
               
@@ -85,93 +86,150 @@ setMethod("PeakFilter", "MSdata",
               if (!is.null(min.nonNAnum.repgroup)) {
                   filt <- abind::abind(along = 3, filt, repapply(function(peakgr, ...) sum(!is.na(peakgr)) >= min.nonNAnum.repgroup))
               }
-			  
-			  if (!is.null(min.nonNApercent)) {
-				  comparison <- apply(.intMatrix, 1, function(peak) sum(!is.na(peak))/length(peak) >= min.nonNApercent)
+              
+              if (!is.null(min.nonNApercent)) {
+                  comparison <- apply(.intMatrix, 1, function(peak) sum(!is.na(peak))/length(peak) >= min.nonNApercent)
                   comparison <- matrix(comparison,
-                                    nrow = length(levels(reps)), 
-                                    ncol = nrow(.intMatrix), 
-                                    byrow = TRUE)
+                                       nrow = length(levels(reps)), 
+                                       ncol = nrow(.intMatrix), 
+                                       byrow = TRUE)
                   filt <- abind::abind(along = 3, filt, comparison)
               }
-			  
-              filtered.peaks <- apply(apply(filt, 2, apply, 1, all, na.rm = FALSE), 2, any, na.rm = FALSE)
-			  msdata <- msdata[filtered.peaks, ]
-			  
-			  if (!is.null(blanks)) msdata <- msdata[ , -blanks]
               
-			  msdata@processLog <- c(msdata@processLog,
-                                     "\n\n", sum(!filtered.peaks), " are filtered")
+              
+              if (dim(filt)[3] == 0) {
+                  msg <- paste0(msg, "\n\nNo data filtering was applied");
+              } else {
+                  filtered.peaks <- apply(apply(filt, 2, apply, 1, all, na.rm = FALSE), 2, any, na.rm = FALSE)
+                  msdata <- msdata[filtered.peaks, ]
+                  msg <- paste0(msg, "\n\nFiltering: ", sum(!filtered.peaks), " are filtered")
+              }
+              
+              if (!is.null(blanks)) {
+                  msdata <- msdata[ , -blanks]
+                  msg <- paste0(msg, "\nBlank samples has been removed from dataset.")
+              }
+              
+              msdata@processLog <- paste0(msdata@processLog, msg)
               return(msdata)
           })
-		  
 
-		  
-# the final variable should be less than 5000 for effective computing
-FilterVariable <- function(filter){
-     int.mat <- as.matrix(dataSet$proc);
-     feat.num <- ncol(int.mat);
-     nm <- NULL;
-    if(filter == "none" && feat.num <= 2000){ # only allow for less than 2000
-        remain <- rep(TRUE, feat.num);
-        #dataSet$proc <<- as.data.frame(int.mat);
-        msg <- "No data filtering was applied";
-    }else{
-        if (filter == "rsd" ){
-            sds <- apply(int.mat, 2, sd, na.rm=T);
-            mns <- apply(int.mat, 2, mean, na.rm=T);
-            filter.val <- abs(sds/mns);
-            nm <- "Relative standard deviation";
-        }else if (filter == "nrsd" ){
-            mads <- apply(int.mat, 2, mad, na.rm=T);
-            meds <- apply(int.mat, 2, median, na.rm=T);
-            filter.val <- abs(mads/meds);
-            nm <- "Non-paramatric relative standard deviation";
-        }else if (filter == "mean"){
-            filter.val <- apply(int.mat, 2, mean, na.rm=T);
-            nm <- "mean";
-        }else if (filter == "sd"){
-            filter.val <- apply(int.mat, 2, sd, na.rm=T);
-            nm <- "standard deviation";
-        }else if (filter == "mad"){
-            filter.val <- apply(int.mat, 2, mad, na.rm=T);
-            nm <- "Median absolute deviation";
-        }else if (filter == "median"){
-            filter.val <- apply(int.mat, 2, median, na.rm=T);
-            nm <- "median";
-        }else{ # iqr
-            filter.val <- apply(int.mat, 2, IQR, na.rm=T);
-            nm <- "Interquantile Range";
-        }
 
-        # get the rank of the
-        rk <- rank(-filter.val, ties.method='random');
 
-        var.num <- ncol(int.mat);
-        if(var.num < 250){ # reduce 5%
-            remain <- rk < var.num*0.95;
-        #    dataSet$proc <<- as.data.frame(int.mat[,rk < var.num*0.95]);
-            msg <- paste("Reduce 5\\% features (", sum(!(rk < var.num*0.95)), ") based on", nm);
-        }else if(ncol(int.mat) < 500){ # reduce 10%
-            remain <- rk < var.num*0.9;
-         #   dataSet$proc <<- as.data.frame(int.mat[,rk < var.num*0.9]);
-            msg <- paste("Reduce 10\\% features (", sum(!(rk < var.num*0.9)), ") based on", nm);
-        }else if(ncol(int.mat) < 1000){ # reduce 25%
-            remain <- rk < var.num*0.75;
-         #   dataSet$proc <<- as.data.frame(int.mat[,rk < var.num*0.75]);
-            msg <- paste("Reduce 25\\% features (", sum(!(rk < var.num*0.75)), ") based on", nm);
-        }else{ # reduce 40%, if still over 5000, then only use top 5000
-            remain <- rk < var.num*0.6;
-            msg <- paste("Reduce 40\\% features (", sum(!remain), ") based on", nm);
-            if(sum(remain) > 5000){
-                remain <-rk < 5000;
-                msg <- paste("Reduced to 5000 features based on", nm);
-            }
-           # dataSet$proc <<- as.data.frame(int.mat[,remain]);
-        }
-    }
 
-    dataSet$remain <<- remain; 
-    dataSet$filter.msg <<- msg;
-    print(msg);
-}
+setGeneric("BasicFilter", 
+           function(msdata, ...) standardGeneric("BasicFilter"))
+		   
+#' Basic peak filtering
+#'
+#' The purpose of the data filtering is to identify and remove variables that are unlikely 
+#' to be of use when modeling the data. No phenotype information are used in the filtering process, 
+#' so the result can be used with any downstream analysis. 
+#' This step is strongly recommended for untargeted metabolomics datasets 
+#' (i.e. spectral binning data, peak lists) with large number of variables, 
+#' many of them are from baseline noises. Filtering can usually improve the results.\cr
+#'
+#' Non-informative variables can be characterized in two groups: \itemize{
+#' \item variables of very small values - can be detected using mean or median; 
+#' \item variables that are near-constant throughout the experiment conditions - 
+#' can be detected using different variance measures.
+#' }
+#'
+#' The following empirical rules are applied during data filtering:\itemize{
+#' \item less than 250 variables: 5\% will be filtered;\cr
+#' \item between 250 - 500 variables: 10\% will be filtered;\cr
+#' \item between 500 - 1000 variables: 25\% will be filtered;\cr
+#' \item over 1000 variables: 40\% will be filtered.
+#' }
+#'
+#' Please note, that \code{"none"} option is only for less than 2000 features. 
+#' Over that, if you choose \code{"none"}, the  IQR filter will still be applied.
+#' 
+#' The maximum allowed number of variables is 5000. 
+#' If over 5000 variables were left after filtering, only the top 5000 will be used in the 
+#' subsequent analysis.
+#' @param msdata \code{\link{MSdata-class}} object to be filtered
+#' @param method Method of filtering one of:\cr
+#' \code{"none"} - no filtering applied\cr
+#' \code{"iqr"} - interquantile range (IQR)\cr
+#' \code{"sd"} - standard deviation (SD)\cr
+#' \code{"mad"} - median absolute deviation (MAD)\cr
+#' \code{"rsd"} - relative standard deviation (RSD = SD/mean)\cr
+#' \code{"nprsd"} - non-parametric relative standard deviation (MAD/median)\cr
+#' \code{"mean"} - mean intensity value\cr
+#' \code{"median"} - median intensity value
+#' none (less than 2000 features)
+#' the final variable should be less than 5000 for effective computing
+#' @return \code{\link{MSdata-class}} object without filtered peaks
+#' @name BasicFilter
+#' @export
+
+setMethod("BasicFilter", "MSdata",
+          function(msdata,
+                   method = "none"){
+              
+              match.arg(method, c("none", "iqr", "sd", "mad", "rsd", "nprsd", "mean", "median"))
+              .intMatrix <- intMatrix(msdata)
+              npks <- nrow(.intMatrix);
+              
+              if (method == "none") {
+                  if (npks <= 2000) {
+                      msg <- "No data filtering was applied"
+                      msdata@processLog <- paste0(msdata@processLog, "\n\n", msg)
+                      return(msdata)
+                  } else 
+                      method <- "iqr"
+              }
+              
+              if (method == "rsd" ){
+                  sds <- apply(.intMatrix, 1, sd, na.rm=T);
+                  mns <- apply(.intMatrix, 1, mean, na.rm=T);
+                  filter.val <- abs(sds/mns);
+                  nm <- "Relative standard deviation";
+              } else if (method == "nprsd" ){
+                  mads <- apply(.intMatrix, 1, mad, na.rm=T);
+                  meds <- apply(.intMatrix, 1, median, na.rm=T);
+                  filter.val <- abs(mads/meds);
+                  nm <- "Non-parametric relative standard deviation";
+              } else if (method == "mean"){
+                  filter.val <- apply(.intMatrix, 1, mean, na.rm=T);
+                  nm <- "mean";
+              } else if (method == "sd"){
+                  filter.val <- apply(.intMatrix, 1, sd, na.rm=T);
+                  nm <- "Standard deviation";
+              } else if (method == "mad"){
+                  filter.val <- apply(.intMatrix, 1, mad, na.rm=T);
+                  nm <- "Median absolute deviation";
+              } else if (method == "median"){
+                  filter.val <- apply(.intMatrix, 1, median, na.rm=T);
+                  nm <- "median";
+              } else if (method == "iqr"){
+                  filter.val <- apply(.intMatrix, 1, IQR, na.rm=T);
+                  nm <- "Interquantile Range";
+              }
+              
+              # get the rank of the
+              rk <- rank(-filter.val, ties.method='random');
+              
+              if(npks < 250){ # reduce 5%
+                  remain <- rk < npks*0.95;
+                  msg <- paste("Reduce 5% features (", sum(!remain), ") based on", nm);
+              } else if(npks < 500){ # reduce 10%
+                  remain <- rk < npks*0.9;
+                  msg <- paste("Reduce 10% features (", sum(!remain), ") based on", nm);
+              } else if(npks < 1000){ # reduce 25%
+                  remain <- rk < npks*0.75;
+                  msg <- paste("Reduce 25% features (", sum(!remain), ") based on", nm);
+              } else{ # reduce 40%, if still over 5000, then only use top 5000
+                  remain <- rk < npks*0.6;
+                  msg <- paste("Reduce 40% features (", sum(!remain), ") based on", nm);
+                  if(sum(remain) > 5000){
+                      remain <- rk < 5000;
+                      msg <- paste("Reduced to 5000 features based on", nm);
+                  }
+              }
+              
+              msdata <- msdata[remain, ]
+              msdata@processLog <- paste0(msdata@processLog, "\n\n", msg)
+              return(msdata)
+          })
