f0c3a50ce6f6b64ca01bffbb76503cd1c471cebe
flajole <dmizubkov@gmail.com>
2016-02-04 14:16:28 +0100

    Normalization by robust peaks is added

--- R/Norm.R
+++ R/Norm.R
@@ -7,19 +7,19 @@ setGeneric("DataNorm",
 #' @param method Method of normalisation. In each sample data are normalised by one of:\cr
 #' \code{"sum"} -  sum concentration of all compounds\cr
 #' \code{"median"} - median concentration of all compounds\cr
-#' \code{"refCompound"} - concentration of reference compound
+#' \code{"refCompound"} - concentration of reference compound\cr
+#' \code{"robustPeaks"} - normalization by robust peaks (see details in Meissner, Steinhauser, Dittmann 2015)
 #' @param ref.cmpd For \code{"refCompound"} method, the name or the order number of reference compound.
 #' 
 #' @return \code{\link{MSdata-class}} object with normalised intensity matrix
-#' @name Norm
 #' @export     	   	
 setMethod("DataNorm", "MSdata",
           function(msdata,
                    method,
                    ref.cmpd = NULL) {
-              match.arg(method, c("sum", "median", "refCompound"))
+              match.arg(method, c("sum", "median", "refCompound", "robustPeaks"))
               .intMatrix <- msdata@intMatrix
-              
+                  
               if (method == "median") {
                   medians <- matrix(apply(.intMatrix, 2, median, na.rm = TRUE),
                                     nrow=nrow(.intMatrix), 
@@ -37,6 +37,24 @@ setMethod("DataNorm", "MSdata",
                   meanInt <- mean(sums)
                   .intMatrix <- .intMatrix * meanInt / sums
                   msg <- "Data are normalised by sample sum" 
+                  
+              } else if (method == "robustPeaks") {
+                  reps <- sampleData(msdata)$ReplicationGroup
+                  tukeys <- .intMatrix
+                  tukeys[ , ] <- 0
+                  for (i in levels(reps)) {
+                      tukeys[, reps==i] <- apply(.intMatrix[, reps==i], 1, tukey)
+                  }
+                  logratios <- log2(.intMatrix/tukeys)
+                  robust <- apply(logratios, 2, tukey.full)
+                  robust <- apply(robust, 1, function(row) !any(is.na(row)))
+                  robust <- logratios[robust, ]
+                  robust <- matrix(2^apply(robust, 2, mean),
+                                 nrow=nrow(.intMatrix), 
+                                 ncol=ncol(.intMatrix), 
+                                 byrow=TRUE)
+                  .intMatrix <- .intMatrix/robust
+                  msg <- "Normalization by robust peaks"
                   
               } else if (method == "refCompound") {
                   if (is.null(ref.cmpd)) 
